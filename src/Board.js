1b3dad69417d7061563a467b8fea60c0be2d8b5b
maddeskoglund <madelein.blomster@hotmail.com>
2018-03-29 11:49:31 +0200

    Updating with Chapter 5
    Now you can add your own notes, delete and edit them!

--- src/Board.js
+++ src/Board.js
@@ -1,38 +1,70 @@
 import React, { Component } from "react";
 import Note from "./Note";
+import FaPlus from "react-icons/lib/fa/plus";
 
 class Board extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      notes: []
+    };
+    this.add = this.add.bind(this);
+    this.eachNote = this.eachNote.bind(this);
+    this.update = this.update.bind(this);
+    this.remove = this.remove.bind(this);
+    this.nextId = this.nextId.bind(this);
+  }
+
+  add(text) {
+    this.setState(prevState => ({
       notes: [
+        ...prevState.notes,
         {
-          id: 33,
-          note: "Call Lisa"
-        },
-        {
-          id: 34,
-          note: "Email John"
-        },
-        {
-          id: 35,
-          note: "Order printer inc"
+          id: this.nextId(),
+          note: text
         }
       ]
-    };
-    this.eachNote = this.eachNote.bind(this);
+    }));
+  }
+
+  nextId() {
+    this.uniqueId = this.uniqueId || 0;
+    return this.uniqueId++;
+  }
+
+  update(newText, i) {
+    console.log("updating item at index", i, newText);
+    this.setState(prevState => ({
+      notes: prevState.notes.map(
+        note => (note.id !== i ? note : { ...note, note: newText })
+      )
+    }));
+  }
+
+  remove(id) {
+    console.log("removing item at", id);
+    this.setState(prevState => ({
+      notes: prevState.notes.filter(note => note.id !== id)
+    }));
   }
 
   eachNote(note, i) {
     return (
-      <Note key={i} index={i}>
+      <Note key={i} index={i} onChange={this.update} onRemove={this.remove}>
         {note.note}
       </Note>
     );
   }
 
   render() {
-    return <div className="board">{this.state.notes.map(this.eachNote)}</div>;
+    return (
+      <div className="board">
+        {this.state.notes.map(this.eachNote)}
+        <button onClick={this.add.bind(null, "New Note")} id="add">
+          <FaPlus />
+        </button>
+      </div>
+    );
   }
 }
 
